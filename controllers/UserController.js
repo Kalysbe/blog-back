8635d3c47d283db57d7788a76cfcba87290deebf
Kalysbe <42235644+Kalysbe@users.noreply.github.com>
2023-10-23 00:56:26 +0600

    heroku2

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -37,7 +37,7 @@ export const register = async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).json({
-      message: 'Не удалось зарегистрироваться',
+      message: 'Не удалось зарегистрироваться!',
     });
   }
 };
