415f2a73f84c35f072853e6d4456cdf6979f2785
Kalysbe <42235644+Kalysbe@users.noreply.github.com>
2024-02-17 18:18:10 +0600

    mongoose

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ import { handleValidationErrors, checkAuth } from './utils/index.js';
 
 import { UserController, PostController } from './controllers/index.js';
 
+mongoose.set('strictQuery', false);
+
 mongoose
   .connect('mongodb+srv://rrkalysbe:123123kk@cluster0.cr98zh3.mongodb.net/blog?retryWrites=true&w=majority')
   .then(() => console.log('DB ok'))
