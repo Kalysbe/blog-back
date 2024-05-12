4d732b347930e4b6923e8aa324b9496a021caa26
Kalysbek <rrkalysbe@gmail.com>
2024-05-13 01:25:52 +0600

    users api for update

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,7 +4,6 @@ import bcrypt from 'bcrypt';
 import UserModel from '../models/User.js';
 
 export const register = async (req, res) => {
-  
   try {
     const password = req.body.password;
     const salt = await bcrypt.genSalt(10);
@@ -43,6 +42,56 @@ export const register = async (req, res) => {
     });
   }
 };
+
+export const update = async (req, res) => {
+  try {
+    const userId = req.params.userId; // Предполагается, что идентификатор пользователя передается через URL параметры
+
+    // Получаем новые данные из запроса
+    const newData = {
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      role: req.body.role,
+    };
+
+    // Если есть новый пароль, хешируем его и обновляем хеш
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(req.body.password, salt);
+      newData.passwordHash = hash;
+    }
+
+    // Обновляем данные пользователя
+    const updatedUser = await UserModel.findByIdAndUpdate(userId, newData, { new: true });
+
+    // Генерируем новый токен
+    const token = jwt.sign(
+      {
+        _id: updatedUser._id,
+      },
+      'secret123',
+      {
+        expiresIn: '30d',
+      },
+    );
+
+    // Исключаем хеш пароля из данных пользователя
+    const { passwordHash, ...userData } = updatedUser._doc;
+
+    // Отправляем обновленные данные и токен в ответе
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось обновить данные пользователя!',
+    });
+  }
+};
+
 
 export const login = async (req, res) => {
   try {
