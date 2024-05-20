ba8d5e10b06b4847e3eaba7e4f860f34540b6b34
Kalysbek <rrkalysbe@gmail.com>
2024-05-20 20:43:31 +0600

    declarations

---
+++ models/Declaration.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const DeclarationSchema = new mongoose.Schema(
+    {
+        code: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: Number,
+            required: true,
+        },
+
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        content: {
+            type: Object,
+            default: {},
+        }
+    },
+    {
+        timestamps: true,
+    },
+);
+
+export default mongoose.model('Declaration', DeclarationSchema);
