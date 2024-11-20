2c094742570ff593edcd1b4e868c95876e3dce1a
Kalysbek <rrkalysbe@gmail.com>
2024-11-20 17:43:56 +0600

    Added api

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-import express from 'express';
-import fs from 'fs';
-import multer from 'multer';
-import cors from 'cors';
-
-import xmlparser from 'express-xml-bodyparser';
-
-import mongoose from 'mongoose';
+const express = require('express');
+const fs = require('fs');
+const multer = require('multer');
+const cors = require('cors');
+const xmlparser = require('express-xml-bodyparser');
+const mongoose = require('mongoose');
+const { registerValidation, loginValidation, postCreateValidation } = require('./validations.js');
+const { handleValidationErrors, checkAuth } = require('./utils/index.js');
+const { UserController, PostController, DeclarationController, ClientController, XmlController } = require('./controllers/index.js');
 
 mongoose.set('strictQuery', false);
-import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
-
-import { handleValidationErrors, checkAuth } from './utils/index.js';
-
-import { UserController, PostController, DeclarationController, ClientController, XmlController } from './controllers/index.js';
 
 mongoose
   .connect('mongodb+srv://rrkalysbe:123123kk@cluster0.cr98zh3.mongodb.net/blog?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -20,6 +16,9 @@ mongoose
   .catch((err) => console.log('DB error', err));
 
 const app = express();
+
+// Устанавливаем базовый путь для API в зависимости от среды
+const apiPrefix = process.env.NODE_ENV === 'production' ? '/api' : '';
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
@@ -39,60 +38,51 @@ app.use(express.json());
 app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
-app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
-app.post('/users',checkAuth, registerValidation, handleValidationErrors, UserController.register);
-app.put('/users/:id',checkAuth,handleValidationErrors, UserController.update,);
-app.delete('/users/:id',checkAuth,handleValidationErrors, UserController.remove,);
-app.get('/auth/me', checkAuth, UserController.getMe);
-app.get('/users', UserController.getAll);
+// Логины и регистрация
+app.post(`${apiPrefix}/auth/login`, loginValidation, handleValidationErrors, UserController.login);
+app.post(`${apiPrefix}/users`, checkAuth, registerValidation, handleValidationErrors, UserController.register);
+app.put(`${apiPrefix}/users/:id`, checkAuth, handleValidationErrors, UserController.update);
+app.delete(`${apiPrefix}/users/:id`, checkAuth, handleValidationErrors, UserController.remove);
+app.get(`${apiPrefix}/auth/me`, checkAuth, UserController.getMe);
+app.get(`${apiPrefix}/users`, UserController.getAll);
 
-app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+// Загрузка файлов
+app.post(`${apiPrefix}/upload`, checkAuth, upload.single('image'), (req, res) => {
   res.json({
     url: `/uploads/${req.file.originalname}`,
   });
 });
 
-app.get('/tags', PostController.getLastTags);
+// Посты
+app.get(`${apiPrefix}/posts`, PostController.getAll);
+app.get(`${apiPrefix}/posts/tags`, PostController.getLastTags);
+app.get(`${apiPrefix}/posts/:id`, PostController.getOne);
+app.post(`${apiPrefix}/posts`, checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
+app.delete(`${apiPrefix}/posts/:id`, checkAuth, PostController.remove);
+app.patch(`${apiPrefix}/posts/:id`, checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
 
-app.get('/posts', PostController.getAll);
-app.get('/posts/tags', PostController.getLastTags);
-app.get('/posts/:id', PostController.getOne);
-app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
-app.delete('/posts/:id', checkAuth, PostController.remove);
-app.patch(
-  '/posts/:id',
-  checkAuth,
-  postCreateValidation,
-  handleValidationErrors,
-  PostController.update,
-);
+// Декларации
+app.get(`${apiPrefix}/declarations`, DeclarationController.getAll);
+app.get(`${apiPrefix}/declarations/:id`, DeclarationController.getOne);
+app.post(`${apiPrefix}/declarations`, checkAuth, handleValidationErrors, DeclarationController.create);
+app.delete(`${apiPrefix}/declarations/:id`, checkAuth, DeclarationController.remove);
+app.put(`${apiPrefix}/declarations/:id`, checkAuth, handleValidationErrors, DeclarationController.update);
 
-//Декларации
-app.get('/declarations', DeclarationController.getAll);
-app.get('/declarations/:id', DeclarationController.getOne);
-app.post('/declarations', checkAuth, handleValidationErrors, DeclarationController.create);
-app.delete('/declarations/:id', checkAuth, DeclarationController.remove);
-app.put(
-  '/declarations/:id',
-  checkAuth,
-  handleValidationErrors,
-  DeclarationController.update,
-)
+// Клиенты
+app.get(`${apiPrefix}/clients`, ClientController.getAll);
+app.get(`${apiPrefix}/clients/:id`, ClientController.getOne);
+app.post(`${apiPrefix}/clients`, handleValidationErrors, ClientController.create);
+app.delete(`${apiPrefix}/clients/:id`, ClientController.remove);
+app.put(`${apiPrefix}/clients/:id`, handleValidationErrors, ClientController.update);
 
-//Клиенты
-app.get('/clients', ClientController.getAll);
-app.get('/clients/:id', ClientController.getOne);
-app.post('/clients', handleValidationErrors, ClientController.create);
-app.delete('/clients/:id',  ClientController.remove);
-app.put('/clients/:id',handleValidationErrors,ClientController.update,)
+// XML
+app.post(`${apiPrefix}/xml`, xmlparser(), XmlController.create);
+app.post(`${apiPrefix}/xml/convert`, xmlparser(), XmlController.converter);
 
-app.post('/xml',   xmlparser(), XmlController.create);
-app.post('/xml/convert',   xmlparser(), XmlController.converter);
-
+// Запуск сервера
 app.listen(process.env.PORT || 4444, (err) => {
   if (err) {
     return console.log(err);
   }
-
   console.log('Server OK');
 });
