c08e70929f2025408d766a2b0c64b4cba88c543d
Kalysbek <rrkalysbe@gmail.com>
2025-01-16 15:50:37 +0600

    Add api for email users

---
+++ controllers/EmailController.js
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+
+
+// Функция для получения списка пользователей с сервера
+const getMailAccountsFromServer = () => {
+    return new Promise((resolve, reject) => {
+      exec('getent passwd', (error, stdout, stderr) => {
+        if (error) {
+          return reject(`Error fetching user list: ${stderr}`);
+        }
+  
+        // Фильтруем пользователей, например, ищем тех, чьи домашние директории содержат "/var/mail/"
+        const accounts = stdout
+          .split('\n')
+          .filter(line => line.includes('/var/mail/'))
+          .map(line => line.split(':')[0]); // Берем только имя пользователя
+  
+        resolve(accounts);
+      });
+    });
+  };
+
+
+export const getAll = async (req, res) => {
+  try {
+
+    const emailAccountsFromServer = await getMailAccountsFromServer();
+    res.json(emailAccountsFromServer);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось получить статьи',
+    });
+  }
+};
