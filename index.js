889316800f25627c5361547acd0e0ced379aaf78
Kalysbek <rrkalysbe@gmail.com>
2025-02-11 17:44:46 +0600

    Changed andpoints

--- index.js
+++ index.js
@@ -39,45 +39,45 @@ app.use('/uploads', express.static('uploads'));
 
 
 
-app.post(`/api/auth/login`, loginValidation, handleValidationErrors, UserController.login);
-app.post(`/api/users`, checkAuth, registerValidation, handleValidationErrors, UserController.register);
-app.put(`/api/users/:id`, checkAuth, handleValidationErrors, UserController.update);
-app.delete(`/api/users/:id`, checkAuth, handleValidationErrors, UserController.remove);
-app.get(`/api/auth/me`, checkAuth, UserController.getMe);
-app.get(`/api/users`, UserController.getAll);
+app.post(`/auth/login`, loginValidation, handleValidationErrors, UserController.login);
+app.post(`/users`, checkAuth, registerValidation, handleValidationErrors, UserController.register);
+app.put(`/users/:id`, checkAuth, handleValidationErrors, UserController.update);
+app.delete(`/users/:id`, checkAuth, handleValidationErrors, UserController.remove);
+app.get(`/auth/me`, checkAuth, UserController.getMe);
+app.get(`/users`, UserController.getAll);
 
-app.post(`/api/upload`, checkAuth, upload.single('image'), (req, res) => {
+app.post(`/upload`, checkAuth, upload.single('image'), (req, res) => {
   res.json({
     url: `/uploads/${req.file.originalname}`,
   });
 });
 
-app.get(`/api/posts`, PostController.getAll);
-app.get(`/api/posts/tags`, PostController.getLastTags);
-app.get(`/api/posts/:id`, PostController.getOne);
-app.post(`/api/posts`, checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
-app.post(`/api/postsGPT`, checkAuth, handleValidationErrors, PostController.createChatGpt);
-app.delete(`/api/posts/:id`, checkAuth, PostController.remove);
-app.patch(`/api/posts/:id`, checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
+app.get(`/posts`, PostController.getAll);
+app.get(`/posts/tags`, PostController.getLastTags);
+app.get(`/posts/:id`, PostController.getOne);
+app.post(`/posts`, checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
+app.post(`/postsGPT`, checkAuth, handleValidationErrors, PostController.createChatGpt);
+app.delete(`/posts/:id`, checkAuth, PostController.remove);
+app.patch(`/posts/:id`, checkAuth, postCreateValidation, handleValidationErrors, PostController.update);
 
-app.get(`/api/declarations`, DeclarationController.getAll);
-app.get(`/api/declarations/:id`, DeclarationController.getOne);
-app.post(`/api/declarations`, checkAuth, handleValidationErrors, DeclarationController.create);
-app.delete(`/api/declarations/:id`, checkAuth, DeclarationController.remove);
-app.put(`/api/declarations/:id`, checkAuth, handleValidationErrors, DeclarationController.update);
+app.get(`/declarations`, DeclarationController.getAll);
+app.get(`/declarations/:id`, DeclarationController.getOne);
+app.post(`/declarations`, checkAuth, handleValidationErrors, DeclarationController.create);
+app.delete(`/declarations/:id`, checkAuth, DeclarationController.remove);
+app.put(`/declarations/:id`, checkAuth, handleValidationErrors, DeclarationController.update);
 
-app.get(`/api/clients`, ClientController.getAll);
-app.get(`/api/clients/:id`, ClientController.getOne);
-app.post(`/api/clients`, handleValidationErrors, ClientController.create);
-app.delete(`/api/clients/:id`, ClientController.remove);
-app.put(`/api/clients/:id`, handleValidationErrors, ClientController.update);
+app.get(`/clients`, ClientController.getAll);
+app.get(`/clients/:id`, ClientController.getOne);
+app.post(`/clients`, handleValidationErrors, ClientController.create);
+app.delete(`/clients/:id`, ClientController.remove);
+app.put(`/clients/:id`, handleValidationErrors, ClientController.update);
 
-app.post('/api/xml', bodyParser.text({ type: 'application/xml' }), XmlController.create);
-app.get(`/api/xml`, XmlController.getAll);
-app.get(`/api/xml/:id`, XmlController.getOne);
-app.post(`/api/xml/convert`, bodyParser.text({ type: 'application/xml' }), XmlController.converter);
+app.post('/xml', bodyParser.text({ type: 'application/xml' }), XmlController.create);
+app.get(`/xml`, XmlController.getAll);
+app.get(`/xml/:id`, XmlController.getOne);
+app.post(`/xml/convert`, bodyParser.text({ type: 'application/xml' }), XmlController.converter);
 
-app.get(`/api/email/users`,EmailController.getAll);
+app.get(`/email/users`,EmailController.getAll);
 
 app.listen(process.env.PORT || 4444, (err) => {
   if (err) {
