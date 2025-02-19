acf50630945cbaada2f2679eb6a055d4a84e4852
Kalysbek <rrkalysbe@gmail.com>
2025-02-19 19:36:58 +0600

    Fixed

---
+++ controllers/PostController.js
@@ -0,0 +1,206 @@
+import PostModel from '../models/Post.js';
+import OpenAI from 'openai';
+import { TelegramBotController } from './index.js'
+
+const openai = new OpenAI({
+ apiKey: process.env.OPENAI_KEY
+});
+
+export const getLastTags = async (req, res) => {
+  try {
+    const posts = await PostModel.find().limit(5).exec();
+
+    const tags = posts
+      .map((obj) => obj.tags)
+      .flat()
+      .slice(0, 5);
+
+    res.json(tags);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось получить тэги',
+    });
+  }
+};
+
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostModel.find().populate('user').sort({ createdAt: -1 }).exec();
+    res.json(posts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось получить статьи',
+    });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: 'after',
+      },
+      (err, doc) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: 'Не удалось вернуть статью',
+          });
+        }
+
+        if (!doc) {
+          return res.status(404).json({
+            message: 'Статья не найдена',
+          });
+        }
+
+        res.json(doc);
+      },
+    ).populate('user');
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось получить статьи',
+    });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    PostModel.findOneAndDelete(
+      {
+        _id: postId,
+      },
+      (err, doc) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: 'Не удалось удалить статью',
+          });
+        }
+
+        if (!doc) {
+          return res.status(404).json({
+            message: 'Статья не найдена',
+          });
+        }
+
+        res.json({
+          success: true,
+        });
+      },
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось получить статьи',
+    });
+  }
+};
+
+export const create = async (req, res) => {
+  try {
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags.split(','),
+      user: req.userId,
+    });
+
+    const post = await doc.save();
+
+    res.json(post);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось создать статью',
+    });
+  }
+};
+
+export const createChatGpt = async (req, res) => {
+  try {
+    const topic = req.body.topic || 'бухгалтерия';
+
+    // Генерация текста
+    const response = await openai.chat.completions.create({
+      model: 'gpt-4o-mini',
+      messages: [
+        {
+          role: 'user',
+          content: `Напиши, пожалуйста, статью про ${topic} в формате Markdown. Обязательно используй заголовки, списки и прочее оформление, чтобы текст хорошо смотрелся в Markdown.`
+        }
+      ],
+    });
+    
+    const generatedText = response.choices[0].message.content;
+  
+
+
+    // Сохранение статьи в базе данных
+    const doc = new PostModel({
+      title: req.body.title || 'Автоматически созданная статья',
+      text: generatedText,
+      imageUrl: req.body.imageUrl || '',
+      tags: req.body.tags ? req.body.tags.split(',') : ['бухгалтерия'],
+      user: req.userId,
+    });
+
+    const post = await doc.save();
+    const urlToArticle = `https://dashboard.adb-solution.com/posts/${post._id}`;
+
+    // Отправляем уведомление в Телеграм
+    await TelegramBotController.sendMessageToChannel({
+      title: req.body.title || 'Новая статья',
+      text: generatedText, // большой текст, мы сократим его внутри функции
+      url: urlToArticle
+    });
+
+    res.json(post);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: 'Не удалось создать статью',
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    await PostModel.updateOne(
+      {
+        _id: postId,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        user: req.userId,
+        tags: req.body.tags.split(','),
+      },
+    );
+
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось обновить статью',
+    });
+  }
+};
